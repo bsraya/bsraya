4ffcd02f000d84f17378898f66deee198b46508b
nathansetyawan96 <nathan.setyawan96@gmail.com>
2022-06-13 12:07:58 +0800

    warning: needs to styled

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,46 @@
-import * as React from 'react'
+import { Flex } from '@chakra-ui/react'
+import Link from 'next/link'
+import styled from '@emotion/styled'
 
 const Footer: React.FC = () => {
+    const link = [
+        {
+            name: 'GitHub',
+            url: 'https://github.com/bsraya',
+        },
+        {
+            name: 'LinkedIn',
+            url: 'https://www.linkedin.com/in/bijonsetyawan/'
+        },
+        {
+            name: 'Email',
+            url: 'nathan.setyawan96@gmail.com'
+        },
+        {
+            name: 'Resume',
+            url: '...'
+        }
+    ]
     return (
-        <div>
-            This is footer
-        </div>
+        <Flex
+            flexDirection="row"
+            alignItems="center"
+            maxWidth="800px"
+            minWidth="356px"
+            width="100%"
+            as="nav"
+            px={[2, 6, 6]}
+            py={2}
+            mt={8}
+            mb={[0, 0, 8]}
+            mx="auto"
+        >
+            {link.map(({ name, url }) => (
+                <Link href={url} key={name} passHref={true}>
+                    <a>{name}</a>
+                </Link>
+            ))}
+        </Flex>
     );
 }
 
