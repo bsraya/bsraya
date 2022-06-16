76aa9ef3f93cf1a0b72498f0b78f9801685eea83
nathansetyawan96 <nathan.setyawan96@gmail.com>
2022-06-16 23:28:17 +0800

    chore: remove paddings, use y-axis margin

--- components/layouts/Page.tsx
+++ components/layouts/Page.tsx
@@ -5,16 +5,12 @@ import { Box, Container } from '@chakra-ui/react'
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     return (
-        <Box
-            as="main"
-            pb={8}
-            mx="auto"
-        >
+        <Box as="main">
             <Seo />
             <Navbar />
             <Container
-                maxW="container.md"
-                pt={14}
+                maxW="container.sm"
+                my={20}
             >
                 {children}
             </Container>
