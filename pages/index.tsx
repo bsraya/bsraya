6c558c318a643f1e7ad703038ceb9d824fa0e6a9
nathansetyawan96 <nathan.setyawan96@gmail.com>
2022-06-17 14:40:16 +0800

    feat: show blogs and portfolios written in mdx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,58 @@
-import type { NextPage } from 'next'
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
 import Layout from '../components/layouts/Page'
-import { Heading, Stack } from '@chakra-ui/react'
+import { Heading, Box, Text } from '@chakra-ui/react'
 
-const Home: NextPage = () => {
+interface Post {
+    frontMatter: {
+        title: string
+        date: string
+        readingTime: string
+        description: string
+        tags: string[]
+        publish: boolean
+    }
+    slug: string
+}
+
+export default function Home ({ posts }: { posts: Post[] }) {
     return (
         <Layout>
-            <Stack align="center">
-                <Heading>This is Home page</Heading>
-            </Stack>
+            <Heading as="h1">This home page</Heading>
+            {
+                posts.map((post) => {
+                    return (
+                        <Box key={post.slug} mb={3}>   
+                            <Text>{post.frontMatter.title}</Text> 
+                            <Text>{post.frontMatter.description}</Text>
+                            <Text>{post.frontMatter.tags}</Text>
+                        </Box>
+                    )
+                })
+            }
         </Layout>
     )
 }
 
-export default Home
+export const getStaticProps = async () => {
+    // get all the files in /content/posts
+    const files = fs.readdirSync(path.join('content', 'posts'))
+    
+    // iterate through all the files in /content/posts
+    const posts = files.map(filename => {
+        const slug = filename.replace('.mdx', '')
+        const content = fs.readFileSync(path.join('content', 'posts', filename), 'utf-8')
+        const { data: frontMatter } = matter(content)
+        return {
+            frontMatter,
+            slug: slug
+        }
+    })
+
+    return {
+        props: {
+            posts
+        }
+    }
+}
--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -1,15 +1,58 @@
-import type { NextPage } from 'next'
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
 import Layout from '../components/layouts/Page'
-import { Heading, Stack } from '@chakra-ui/react'
+import { Box, Heading, Text } from '@chakra-ui/react'
 
-const Portfolio: NextPage = () => {
+interface Portfolio {
+    frontMatter: {
+        title: string
+        date: string
+        readingTime: string
+        description: string
+        tags: string[]
+        publish: boolean
+    }
+    slug: string
+}
+
+export default function Portfolio({portfolios}: {portfolios: Portfolio[]}) {
     return (
         <Layout>
-            <Stack align="center">
-                <Heading>This is Portfolio page</Heading>
-            </Stack>
+            <Heading>This is Portfolio page</Heading>
+            {
+                portfolios.map((portfolio) => {
+                    return (
+                        <Box key={portfolio.slug} mb={3}>   
+                            <Text>{portfolio.frontMatter.title}</Text> 
+                            <Text>{portfolio.frontMatter.description}</Text>
+                            <Text>{portfolio.frontMatter.tags}</Text>
+                        </Box>
+                    )
+                })
+            }
         </Layout>
     )
 }
 
-export default Portfolio
+export const getStaticProps = async () => {
+    // get all the files in /content/posts
+    const files = fs.readdirSync(path.join('content', 'portfolios'))
+    
+    // iterate through all the files in /content/posts
+    const portfolios = files.map(filename => {
+        const slug = filename.replace('.mdx', '')
+        const content = fs.readFileSync(path.join('content', 'portfolios', filename), 'utf-8')
+        const { data: frontMatter } = matter(content)
+        return {
+            frontMatter,
+            slug: slug
+        }
+    })
+
+    return {
+        props: {
+            portfolios
+        }
+    }
+}
