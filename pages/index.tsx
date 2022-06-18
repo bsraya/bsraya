64f01e0c4652445b3cf9df7bd4069707b1c093ab
Bijon Setyawan Raya <nathan.setyawan96@gmail.com>
2022-06-18 23:56:56 +0800

    feat: optimize and refactor

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,76 +1,16 @@
 import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
-import NextLink from 'next/link'
-import Layout from '../components/layouts/Page'
-import { Button, Flex, Heading, LinkBox, LinkOverlay, Text, useColorModeValue } from '@chakra-ui/react'
+import Posts from '../components/Posts'
+import Layout from '../components/Layout'
+import { Heading } from '@chakra-ui/react'
+import type { IPost } from '../types/post.type'
 
-interface Post {
-    frontMatter: {
-        title: string
-        date: string
-        readingTime: string
-        description: string
-        tags: string[]
-        publish: boolean
-    }
-    slug: string
-}
-
-export default function Home({ posts }: { posts: Post[] }) {
-    const colors = ['gray', 'red', 'orange', 'yellow', 'green', 'teal', 'blue', 'cyan', 'purple', 'pink']
+export default function Home({ posts }: { posts: IPost[] }): JSX.Element {
     return (
         <Layout>
             <Heading as="h2" size="md" mb={3}>Recent posts</Heading>
-            {
-                posts.map((post) => {
-                    return (
-                        <LinkBox as="article" p='5' my={5} borderWidth='1px' rounded="md" key={post.slug}>   
-                            <Text
-                                fontSize="sm"
-                                color="gray.500"
-                            >
-                                {post.frontMatter.date} - {post.frontMatter.readingTime} reading time
-                            </Text>
-                            <NextLink href={'/blog/' + post.slug} passHref>
-                                <LinkOverlay >
-                                    <Heading as="h1">
-                                        {post.frontMatter.title}
-                                    </Heading>
-                                </LinkOverlay>  
-                            </NextLink>
-                            <Text as="p" my={5}>{post.frontMatter.description}</Text>
-                            <Flex>
-                                {
-                                    post.frontMatter.tags.map((tag) => {
-                                        var randomColor = colors[Math.floor(Math.random() * colors.length)]
-                                        return (
-                                            <NextLink
-                                                href={'/tag/' + tag}
-                                                key={tag}
-                                                passHref
-                                            >
-                                                <Button
-                                                    mr={2}
-                                                    background={useColorModeValue(`white`, 'rgba(26,32,44)')}
-                                                    _hover={{
-                                                        bg: `${randomColor}.100`,
-                                                        color: 'rgba(26,32,44)',
-                                                        borderColor: `${randomColor}.700`,
-                                                    }}
-                                                >
-                                                    <Text color={randomColor+'.700'}>#</Text>
-                                                    <Text>{tag}</Text>
-                                                </Button>
-                                            </NextLink>
-                                        )
-                                    })
-                                }
-                            </Flex>
-                        </LinkBox>
-                    )
-                })
-            }
+            <Posts posts={posts} />
         </Layout>
     )
 }
--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -1,74 +1,16 @@
 import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
-import Layout from '../components/layouts/Page'
-import { Flex, LinkBox, LinkOverlay, Heading, Text, Button, useColorModeValue } from '@chakra-ui/react'
-import NextLink from 'next/link'
+import Layout from '../components/Layout'
+import Posts from './../components/Posts'
+import { Heading } from '@chakra-ui/react'
+import type { IPost } from '../types/post.type'
 
-interface Portfolio {
-    frontMatter: {
-        title: string
-        date: string
-        readingTime: string
-        description: string
-        tags: string[]
-        publish: boolean
-    }
-    slug: string
-}
-
-export default function Portfolio({ portfolios }: { portfolios: Portfolio[] }) {
-    const colors = ['gray', 'red', 'orange', 'yellow', 'green', 'teal', 'blue', 'cyan', 'purple', 'pink']
+export default function Portfolio({ portfolios }: { portfolios: IPost[] }): JSX.Element {
     return (
         <Layout>
             <Heading as="h2" size="md" mb={3}>Portfolio</Heading>
-            {
-                portfolios.map((portfolio) => {
-                    return (
-                        <LinkBox as="article" p='5' my={5} borderWidth='1px' rounded="md" key={portfolio.slug} >   
-                            <Text
-                                fontSize="sm"
-                                color="gray.500"
-                            >
-                                {portfolio.frontMatter.date} - {portfolio.frontMatter.readingTime} reading time
-                            </Text>
-                            <NextLink href={'/portfolio/' + portfolio.slug} passHref>
-                                <LinkOverlay >
-                                    <Heading as="h1">
-                                        {portfolio.frontMatter.title}
-                                    </Heading>
-                                </LinkOverlay>  
-                            </NextLink>
-                            <Text my={5}>{portfolio.frontMatter.description}</Text>
-                            <Flex>
-                                {
-                                    portfolio.frontMatter.tags.map((tag) => {
-                                        var randomColor = colors[Math.floor(Math.random() * colors.length)]
-                                        return (
-                                            <Button
-                                                key={tag}
-                                                mr={2}
-                                                border='1px'
-                                                borderColor={randomColor+'.700'}
-                                                _hover={{
-                                                    bg: `${randomColor}.100`,
-                                                    color: `${randomColor}.700`,
-                                                }}
-                                            >
-                                                <Text
-                                                    // get random color from "colors" array
-                                                    color={useColorModeValue(`${randomColor}.700`, `${randomColor}.200`)}
-                                                >#</Text>
-                                                <Text>{tag}</Text>
-                                            </Button>
-                                        )
-                                    })
-                                }
-                            </Flex>
-                        </LinkBox>
-                    )
-                })
-            }
+            <Posts posts={portfolios} />
         </Layout>
     )
 }
