0fa0fcf6ff31ee65bf7f6c5dbb0ca2e88fcabfe1
Bijon Setyawan Raya <nathan.setyawan96@gmail.com>
2022-06-19 20:30:09 +0800

    chore: add necessary config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,20 @@
 module.exports = async () => {
     /**
      * @type {import('next').NextConfig}
-     */
-    const nextConfig = {
+    */
+    const withBundleAnalyzer = require('@next/bundle-analyzer')({
+        enabled: process.env.ANALYZE === 'true'
+    });
+    const nextConfig = withBundleAnalyzer({
         optimizeFonts: true,
         ignoreBuildErrors: false,
         reactStrictMode: true,
+        eslint: {
+            dirs: ['components', 'content', 'pages', 'types', 'utils']
+        },
         compiler: {
             emotion: true,
         },
-    }
+    })
     return nextConfig
 }
