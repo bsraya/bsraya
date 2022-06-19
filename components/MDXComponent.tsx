a8882ae6821831070fbea77c729a4c4385453ae1
Bijon Setyawan Raya <nathan.setyawan96@gmail.com>
2022-06-19 20:31:02 +0800

    style: set mdx styling

---
+++ components/MDXComponent.tsx
@@ -0,0 +1,187 @@
+import {
+    Box,
+    
+    List,
+    ListItem,
+    Heading,
+    Link,
+    Text,
+    Divider,
+    useColorMode,
+    Table,
+    TableContainer,
+    Kbd,
+    Thead,
+    Tbody,
+    Tfoot,
+    Tr,
+    Th,
+    Td,
+    TableCaption,
+    Alert,
+    Stack,
+    AlertIcon,
+} from '@chakra-ui/react';
+import NextLink from 'next/link';
+import Image from 'next/image';
+
+const CustomImage = (props: any) => {
+    return (
+        <Image
+            width={props.width}
+            height={props.height}
+            src={props.src}
+            alt={props.alt}
+            layout="responsive" 
+        />
+    );
+};
+
+const CustomLink = (props: any) => {
+    const { colorMode } = useColorMode();
+    const color = {
+        light: 'blue.500',
+        dark: 'blue.500'
+    };
+
+    const href = props.href;
+    const isInternalLink = href && (href.startsWith('/') || href.startsWith('#'));
+
+    if (isInternalLink) {
+        return (
+        <NextLink href={href} passHref>
+            <Link color={color[colorMode]} {...props} />
+        </NextLink>
+        );
+    }
+
+    return <Link color={color[colorMode]} isExternal {...props} />;
+};
+
+const Quote = (props: any) => {
+    const { colorMode } = useColorMode();
+    const bgColor = {
+        light: 'blue.50',
+        dark: 'blue.900'
+    };
+
+    return (
+        <Alert
+        mt={4}
+        w="98%"
+        bg={bgColor[colorMode]}
+        variant="left-accent"
+        status="info"
+        css={{
+            '> *:first-of-type': {
+            marginTop: 0,
+            marginLeft: 8
+            }
+        }}
+        {...props}
+        />
+    );
+};
+
+const DocsHeading = (props: any) => (
+    <Heading
+        css={{
+        scrollMarginTop: '100px',
+        scrollSnapMargin: '100px', // Safari
+        '&[id]': {
+            pointerEvents: 'none'
+        },
+        '&[id]:before': {
+            display: 'block',
+            height: ' 6rem',
+            marginTop: '-6rem',
+            visibility: 'hidden',
+            content: `""`
+        },
+        '&[id]:hover a': { opacity: 1 }
+        }}
+        {...props}
+        mb="1em"
+        mt="2em"
+    >
+        <Box pointerEvents="auto">
+        {props.children}
+        {props.id && (
+            <Box
+            aria-label="anchor"
+            as="a"
+            color="blue.500"
+            fontWeight="normal"
+            outline="none"
+            _focus={{
+                opacity: 1,
+                boxShadow: 'outline'
+            }}
+            opacity="0"
+            ml="0.375rem"
+            href={`#${props.id}`}
+            >
+            #
+            </Box>
+        )}
+        </Box>
+    </Heading>
+    );
+
+const Hr = () => {
+    const { colorMode } = useColorMode();
+    const borderColor = {
+        light: 'gray.400',
+        dark: 'gray.600'
+    };
+
+    return <Divider borderColor={borderColor[colorMode]} my={4} w="100%" />;
+};
+
+const MDXComponent = {
+    i: (p: any) => <Text as="i" {...p} />,
+    small: (p: any) => <Text as="small" {...p} />,
+    br: (props: any) => <Box height="24px" {...props} />,
+    p: (props: any) => <Text as="p" my={5}  lineHeight="tall" {...props} />,
+    strong: (p: any) => <Text as="strong" fontWeight="semibold" {...p} />,
+    
+    li: (p: any) => <ListItem ml={4} {...p} />,
+    ul: (p: any) => <List styleType="disc" spacing={1} {...p} />,
+    ol: (p: any) => <List as="ol" styleType="decimal" spacing={1} {...p} />,
+    
+    h1: (props: any) => <Heading as="h1" size="xl" my={4} {...props} />,
+    h2: (props: any) => (
+        <DocsHeading as="h2" size="lg" fontWeight="bold" {...props} />
+    ),
+    h3: (props: any) => (
+        <DocsHeading as="h3" size="md" fontWeight="bold" {...props} />
+    ),
+    h4: (props: any) => (
+        <DocsHeading as="h4" size="sm" fontWeight="bold" {...props} />
+    ),
+    h5: (props: any) => (
+        <DocsHeading as="h5" size="sm" fontWeight="bold" {...props} />
+    ),
+    h6: (props: any) => (
+        <DocsHeading as="h6" size="xs" fontWeight="bold" {...props} />
+    ),
+    
+    hr: Hr,
+    a: CustomLink,
+    blockquote: Quote,
+    image: CustomImage,
+    Alert: (props: any) => <Alert my={5} borderRadius="0.5rem" type="left-accent" {...props} />,
+    table: (props: any) => <TableContainer><Table variant='striped' colorScheme='teal' {...props} /></TableContainer>,
+    Kbd,
+    Thead,
+    Tbody,
+    Tfoot,
+    Tr,
+    Th,
+    Td,
+    TableCaption,
+    Stack,
+    AlertIcon,
+};
+
+export default MDXComponent;
--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,14 +1,31 @@
 import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
-import Layout from '../../components/Layout'
 import { Heading, Text } from '@chakra-ui/react'
 import { MDXRemote } from 'next-mdx-remote'
 import { serialize } from 'next-mdx-remote/serialize'
+
+// components
 import Tags from './../../components/Tags'
+import Layout from '../../components/Layout'
+import MDXComponents from '../../components/MDXComponent'
+
+// interface 
 import type { IMdxPage } from '../../types/mdx.type'
 
-export default function Blog ({ frontMatter, mdxSource }: IMdxPage): JSX.Element {
+// remark plugins
+import remarkGfm from 'remark-gfm'
+import remarkMath from 'remark-math'
+
+// rehype pluings
+import rehypeSlug from 'rehype-slug'
+import rehypeKatex from 'rehype-katex'
+import rehypePrismPlus from 'rehype-prism-plus'
+import rehypeCodeTitles from 'rehype-code-titles'
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+import rehypePrismDiff from 'rehype-prism-diff'
+
+export default function Blog({ frontMatter, mdxSource }: IMdxPage): JSX.Element {
     return (
         <Layout>
             <Text
@@ -17,9 +34,9 @@ export default function Blog ({ frontMatter, mdxSource }: IMdxPage): JSX.Element
             >
                 {frontMatter.date} - {frontMatter.readingTime} reading
             </Text>
-            <Heading as="h1">{frontMatter.title}</Heading>
+            <Heading as="h1" size='3xl' my={5}>{frontMatter.title}</Heading>
             <Tags tags={frontMatter.tags} />
-            <MDXRemote {...mdxSource} components={{}} />
+            <MDXRemote {...mdxSource} components={MDXComponents} />
         </Layout>
     )
 }
@@ -46,7 +63,26 @@ export const getStaticProps = async ( { params: { slug } }: { params: { slug: st
 
     const { data: frontMatter, content } = matter(markdownWithMeta)
 
-    const mdxSource = await serialize(content)
+    const mdxSource = await serialize(
+        content, 
+        {
+            mdxOptions: {
+                remarkPlugins: [
+                    remarkGfm,
+                    remarkMath,
+                ],
+                rehypePlugins: [
+                    rehypeSlug,
+                    rehypeCodeTitles,
+                    [rehypeAutolinkHeadings, { behavior: 'wrap' }],
+                    rehypeKatex,
+                    rehypePrismPlus,
+                    rehypePrismDiff
+                ],
+                format: 'mdx'
+            }
+        }
+    )
 
     return {
         props: {
