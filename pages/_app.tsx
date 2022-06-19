e0a297b7b3d98c59f38350a7d4196e660ed15898
Bijon Setyawan Raya <nathan.setyawan96@gmail.com>
2022-06-19 20:31:45 +0800

    style: set mdx styling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,10 @@
-import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
-
-import { extendTheme } from '@chakra-ui/react'
-
-// 2. Extend the theme to include custom colors, fonts, etc
-const colors = {
-    brand: {
-        900: '#1a365d',
-        800: '#153e75',
-        700: '#2a69ac',
-    },
-}
-
-const theme = extendTheme({ colors })
+import { ChakraProvider } from '@chakra-ui/react'
+import 'katex/dist/katex.min.css'
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
     return (
-        <ChakraProvider theme={theme}>
+        <ChakraProvider>
             <Component {...pageProps} />
         </ChakraProvider>
     )
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,6 +10,7 @@ export default class MyDocument extends NextDocument {
                         href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
                         rel="stylesheet"
                     />
+                    <link rel="stylesheet" href="https://unpkg.com/dracula-prism/dist/css/dracula-prism.min.css" />
                 </Head>
                 <body>
                     <ColorModeScript />
