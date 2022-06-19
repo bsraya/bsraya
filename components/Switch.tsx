ffd749e19b86834335287be1bfefa781fddb93da
Bijon Setyawan Raya <nathan.setyawan96@gmail.com>
2022-06-19 22:49:52 +0800

    fix: dark/light mode button shape

--- components/Switch.tsx
+++ components/Switch.tsx
@@ -1,17 +1,15 @@
-import { Button, useColorMode, useColorModeValue } from '@chakra-ui/react'
+import { useColorMode, IconButton, IconButtonProps } from '@chakra-ui/react'
 import { MoonIcon, SunIcon } from '@chakra-ui/icons'
 
-export default function Switch(): JSX.Element{
+export default function ColorModeSwitch({...props}): JSX.Element {
     const { colorMode, toggleColorMode } = useColorMode()
     return (
-        <Button
-            onClick={toggleColorMode}
-            backgroundColor={
-                useColorModeValue('gray.200', 'gray.700')
-            }
-            mx={2}
-        >
-            {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
-        </Button>
+        <IconButton
+            aria-label={colorMode === 'dark' ? 'Dark Mode' : 'Light Mode'}
+            icon={colorMode === 'dark' ? <MoonIcon /> : <SunIcon />}
+            isRound
+            onMouseDown={toggleColorMode}
+            {...props}
+        />
     )
 }
