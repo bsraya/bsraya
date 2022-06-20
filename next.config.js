48f72d581e83b741e5ba4c42453b3ddcfe0312dc
nathansetyawan96 <nathan.setyawan96@gmail.com>
2022-06-20 12:54:19 +0800

    chore: disable PWA for now

--- next.config.js
+++ next.config.js
@@ -1,20 +1,32 @@
-module.exports = async () => {
-    /**
-     * @type {import('next').NextConfig}
-    */
-    const withBundleAnalyzer = require('@next/bundle-analyzer')({
-        enabled: process.env.ANALYZE === 'true'
-    });
-    const nextConfig = withBundleAnalyzer({
-        optimizeFonts: true,
-        ignoreBuildErrors: false,
-        reactStrictMode: true,
-        eslint: {
-            dirs: ['components', 'content', 'pages', 'types', 'utils']
-        },
-        compiler: {
-            emotion: true,
-        },
-    })
-    return nextConfig
+/**
+ * @type {import('next').NextConfig}
+*/
+const withPlugins = require('next-compose-plugins')
+
+const nextConfig = {
+    optimizeFonts: true,
+    ignoreBuildErrors: false,
+    reactStrictMode: true,
+    eslint: {
+        dirs: ['components', 'content', 'pages', 'types', 'utils']
+    },
+    compiler: {
+        emotion: true,
+    },
 }
+
+// const runtimeCaching = require('next-pwa/cache')
+// const withPWA = require('next-pwa')({
+//     pwa: {
+//         dest: "public",
+//         register: true,
+//         skipWaiting: true,
+//         disable: process.env.NODE_ENV === 'development',
+//         runtimeCaching,
+//     }
+// })
+
+module.exports = withPlugins([
+    nextConfig,
+    // withPWA,
+])
