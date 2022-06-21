fe9a140ca20d35c14fbed7fb65acf1cac311e483
Bijon Setyawan Raya <nathan.setyawan96@gmail.com>
2022-06-21 22:08:13 +0800

    style: widen menu links' margin

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -79,12 +79,12 @@ export default function Navigation(): JSX.Element {
                                     </Center>
                                     <br />
                                     <MenuDivider />
-                                    <MenuItem>
+                                    <MenuItem my={5} _hover={{ backgroundColor: 'transparent' }}>
                                         <Link href="/about">
                                             About
                                         </Link>
                                     </MenuItem>
-                                    <MenuItem>
+                                    <MenuItem my={5} _hover={{ backgroundColor: 'transparent' }}>
                                         <Link href="/portfolio">
                                             Portfolio
                                         </Link>
