19094d1ce51e1e2ec2b77417265e1403a5935b85
Bijon Setyawan Raya <nathan.setyawan96@gmail.com>
2022-06-21 20:56:27 +0800

    chore: disable random color

--- components/Tags.tsx
+++ components/Tags.tsx
@@ -1,7 +1,5 @@
-import { useState } from 'react';
-import { Button, Flex, Text } from '@chakra-ui/react'
 import NextLink from 'next/link'
-import { Color } from '../utils/color'
+import { Button, Flex, Text, useColorModeValue} from '@chakra-ui/react'
 
 // pass props as an object
 export default function Tags({ tags }: { tags: string[] }) {
@@ -9,7 +7,6 @@ export default function Tags({ tags }: { tags: string[] }) {
         <Flex>
             {
                 tags.map((tag: string, index: any) => {
-                    var randomColor = Color()
                     return (
                         <NextLink
                             href={'/tag/' + tag}
@@ -20,13 +17,10 @@ export default function Tags({ tags }: { tags: string[] }) {
                                 mr={2}
                                 variant="ghost"
                                 _hover={{
-                                    // append bgColor[number] with .100 to get a random color
-                                    bg: 'blue.100',
-                                    color: 'rgba(26,32,44)',
-                                    borderColor: `${randomColor}.700`,
+                                    bg: useColorModeValue('blue.200', 'blue.600'),
                                 }}
                             >
-                                <Text color='blue.600'>#</Text>
+                                <Text color={useColorModeValue('blue.600', 'blue.200')}>#</Text>
                                 <Text>{tag}</Text>
                             </Button>
                         </NextLink>
