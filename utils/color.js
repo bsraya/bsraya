6a58ce1238cc10cdba28c7b6565d2a66c304659d
Bijon Setyawan Raya <nathan.setyawan96@gmail.com>
2022-06-21 20:54:24 +0800

    chore: delete unused files

--- utils/color.js
+++
@@ -1,4 +0,0 @@
-export function Color() {
-    const colors = ['gray', 'red', 'orange', 'yellow', 'green', 'teal', 'blue', 'cyan', 'purple', 'pink']
-    return colors[Math.floor(Math.random() * colors.length)]
-}
