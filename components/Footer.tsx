b4996947f9f4a4f11610847a1f4ba6723af479a7
Bijon Setyawan Raya <nathan.setyawan96@gmail.com>
2022-06-21 20:55:06 +0800

    feat: change icons

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,20 +1,23 @@
-import { Flex, Link, useColorModeValue } from '@chakra-ui/react'
-import { BsGithub, BsLinkedin } from 'react-icons/bs'
-import { EmailIcon } from '@chakra-ui/icons'
+import { Flex, Link, IconButton } from '@chakra-ui/react'
+import { FiGithub, FiLinkedin } from 'react-icons/fi'
+import { MdOutlineMail } from 'react-icons/md'
 
 export default function Footer(): JSX.Element {
     const size = '1.5rem'
     const link = [
         {
-            icon: <BsGithub style={{ fontSize: size }} />,
+            key: 'github',
+            icon: <FiGithub style={{ fontSize: size }} />,
             url: 'https://github.com/bsraya',
         },
         {
-            icon: <BsLinkedin style={{ fontSize: size }} />,
+            key: 'linkedin',
+            icon: <FiLinkedin style={{ fontSize: size }} />,
             url: 'https://www.linkedin.com/in/bijonsetyawan/'
         },
         {
-            icon: <EmailIcon style={{ fontSize: size }} />,
+            key: 'email',
+            icon: <MdOutlineMail style={{ fontSize: size }} />,
             url: 'mailto:nathan.setyawan96@gmail.com',
         },
     ]
@@ -22,21 +25,24 @@ export default function Footer(): JSX.Element {
     return (
         <Flex as="footer" marginBottom={20} direction="column" align="center">
             <Flex maxW="container.md">
-                {link.map((item, index) => (
+                {link.map((item) => (
                     <Link
-                        key={index}
+                        key={item.key}
                         as="a"
                         px={4}
                         py={2}
-                        rounded={'md'}
-                        _hover={{
-                            textDecoration: 'none',
-                            bg: 'blue.500',
-                        }}
+                        rounded={'xl'}
                         href={item.url}
                         isExternal
                     >
-                        {item.icon}
+                        <IconButton
+                            key={item.key}
+                            aria-label={item.key}
+                            icon={item.icon}
+                            variant="ghost"
+                            isRound
+                            mr={5}
+                        />
                     </Link>
                 ))}
             </Flex>
