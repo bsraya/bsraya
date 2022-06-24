a4f6db819811f0ae162bb001a22a91b358efa91b
nathansetyawan96 <nathan.setyawan96@gmail.com>
2022-06-25 01:59:45 +0800

    feat: hide non-published posts

--- components/Posts.tsx
+++ components/Posts.tsx
@@ -10,49 +10,51 @@ export default function Posts({ posts, type }: { posts: IPost[]; type: string })
         <>
             {
                 posts.map((post) => {
-                    return (
-                        <LinkBox
-                            as="article"
-                            p='5'
-                            my={5}
-                            borderWidth='2px'
-                            rounded="md"
-                            key={post.slug}
-                            borderColor="transparent"
-                            _hover={{
-                                border: '2px solid',
-                                borderColor: randomColor + ".500",
-                                boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)',
-                                transition: 'all 0.2s ease-in-out',
-                                transform: 'translateY(-10px)',
-                            }}
-                        >   
-                            <Text
-                                fontSize="sm"
-                                color="gray.500"
-                            >
-                                {post.frontMatter.date} - {post.frontMatter.readingTime} reading
-                            </Text>
-                            <NextLink href={'/'+ type + '/' + post.slug} passHref>
-                                <LinkOverlay >
-                                    <Heading as="h1">
-                                        {post.frontMatter.title}
-                                    </Heading>
-                                </LinkOverlay>  
-                            </NextLink>
-                            <Text as="p" my={5}>{post.frontMatter.description}</Text>
-                            {
-                                post.frontMatter.tags && (
-                                    <Tags
-                                        tags={
-                                            post.frontMatter.tags
-                                        }
-                                        color={randomColor}
-                                    />
-                                )
-                            }
-                        </LinkBox>
-                    )
+                    if (post.frontMatter.publish === true) {
+                        return(
+                            <LinkBox
+                                as="article"
+                                p='5'
+                                my={5}
+                                borderWidth='2px'
+                                rounded="md"
+                                key={post.slug}
+                                borderColor="transparent"
+                                _hover={{
+                                    border: '2px solid',
+                                    borderColor: randomColor + ".500",
+                                    boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)',
+                                    transition: 'all 0.2s ease-in-out',
+                                    transform: 'translateY(-10px)',
+                                }}
+                            >   
+                                <Text
+                                    fontSize="sm"
+                                    color="gray.500"
+                                >
+                                    {post.frontMatter.date} - {post.frontMatter.readingTime} reading
+                                </Text>
+                                <NextLink href={'/'+ type + '/' + post.slug} passHref>
+                                    <LinkOverlay >
+                                        <Heading as="h1">
+                                            {post.frontMatter.title}
+                                        </Heading>
+                                    </LinkOverlay>  
+                                </NextLink>
+                                <Text as="p" my={5}>{post.frontMatter.description}</Text>
+                                {
+                                    post.frontMatter.tags && (
+                                        <Tags
+                                            tags={
+                                                post.frontMatter.tags
+                                            }
+                                            color={randomColor}
+                                        />
+                                    )
+                                }
+                            </LinkBox>
+                        )
+                    }
                 })
             }
         </>
