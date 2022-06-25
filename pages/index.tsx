1247c5cf7d62cd0abf2fe048d21460dceda68d55
Bijon Setyawan Raya <nathan.setyawan96@gmail.com>
2022-06-25 22:52:52 +0800

    feat: sort and show the first 3 recent posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,24 +5,22 @@ import Posts from '../components/Posts'
 import Layout from '../components/Layout'
 import type { IPost } from '../types/post.type'
 import NextLink from 'next/link'
-import { Alert, AlertIcon, Box, Button, Flex, Heading, Text } from '@chakra-ui/react'
+import { DateTime } from 'luxon'
+import { Alert, AlertIcon, Box, Button, Heading, Text, Link, Flex } from '@chakra-ui/react'
+import { ArrowForwardIcon } from '@chakra-ui/icons'
 
 export default function Home({ posts }: { posts: IPost[] }): JSX.Element {
     return (
         <Layout>
-            <Heading as="h1">Bijon Setyawan Raya</Heading>
+            <Heading as="h1" mb={2}>Bijon Setyawan Raya</Heading>
             <Text>Graduate student at National Tsing Hua University</Text>
 
             <Alert mt={10} variant="left-accent" status='info'>
-                {/* make AlertIcon stays on top */}
                 <AlertIcon
-                    mt={3}
+                    mt={1}
                     mb="auto"
                 />
                 <Box lineHeight="taller">
-                    {/* write
-                        "I am currently a TA in a **Linear Algebra** course at **National Tsing Hua University**."
-                    */}
                     I am currently a TA in a <Text display="inline-flex" fontWeight="bold">Linear Algebra</Text> course at National Tsing Hua University.
                     <Text>
                         Please click on the link below to see some of the topics I have covered.
@@ -40,16 +38,23 @@ export default function Home({ posts }: { posts: IPost[] }): JSX.Element {
 
             <Heading as="h2" size="md" mt={10} mb={3}>Recent posts</Heading>
             <Posts posts={posts} type="blog" />
+            <NextLink href="/blog" passHref>
+                <Link _hover={{textDecoration: "none"}}>
+                    <Flex >
+                        <Text >Read all posts</Text><ArrowForwardIcon ml={2} my="auto" />
+                    </Flex>
+                </Link>
+            </NextLink>
         </Layout>
     )
 }
 
 export const getStaticProps = async () => {
-    // get all folders' in content/portfolios
+    // get the name of all folders in /content/posts
     const folders = fs.readdirSync(path.join(process.cwd(), 'content', 'posts'))
-    
+
     // iterate through all the files in /content/posts
-    const posts = folders.map(slug => {
+    var posts = folders.map(slug => {
         const content = fs.readFileSync(path.join('content', 'posts', slug, 'index.mdx'), 'utf-8')
         const { data: frontMatter } = matter(content)
         return {
@@ -57,6 +62,16 @@ export const getStaticProps = async () => {
             slug: slug
         }
     })
+
+    // sort the posts by date
+    posts.sort((a, b) => {
+        const aDate = DateTime.fromFormat(a.frontMatter.date, "LLLL dd, yyyy")
+        const bDate = DateTime.fromFormat(b.frontMatter.date, "LLLL dd, yyyy")
+        return bDate - aDate
+    })
+
+    // get the first three posts
+    posts = posts.slice(0,3)
 
     return {
         props: {
