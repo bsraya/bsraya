b200ded898e569dc8e284e35f820bdb497fa0e2a
Bijon Setyawan Raya <nathan.setyawan96@gmail.com>
2022-06-29 22:32:38 +0800

    remove

--- lib/color.ts
+++
@@ -1,4 +0,0 @@
-export function Color(): string {
-    const colors: string[] = ['red', 'orange', 'yellow', 'green', 'teal', 'blue', 'cyan', 'purple', 'pink']
-    return colors[Math.floor(Math.random() * colors.length)]
-}
