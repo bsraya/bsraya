f3293dcca42c876cd6d8e765e86b15f45f4e4cb0
nathansetyawan96 <nathan.setyawan96@gmail.com>
2022-07-02 02:50:03 +0800

    style: fix margins

---
+++ pages/_404.tsx
@@ -0,0 +1,12 @@
+import { Heading } from '@chakra-ui/react'
+import Layout from '../components/Layout'
+
+function _404(): JSX.Element {
+    return (
+        <Layout>
+            <Heading as="h1">404</Heading>
+        </Layout>
+    )
+}
+
+export default _404
