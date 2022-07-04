ee996c7a74e5cad0e2009670f7afb0a0da0b14f7
nathansetyawan96 <nathan.setyawan96@gmail.com>
2022-07-04 15:02:58 +0800

    feat: add 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,10 +1,13 @@
-import { Heading } from '@chakra-ui/react'
+import { Heading, Text, Button } from '@chakra-ui/react'
 import Layout from '../components/Layout'
+import NextLink from 'next/link'
 
 function _404(): JSX.Element {
     return (
         <Layout>
-            <Heading as="h1">404</Heading>
+            <Heading as="h1">404 Not Found</Heading>
+            <Text my={5}>Chill ... You are just lost</Text>
+            <Button colorScheme="blue"><NextLink href="/">Go to Home</NextLink></Button>
         </Layout>
     )
 }
