0f0219cc8b6d5b7bd2fa848a4adc415a608d13a4
Bijon Setyawan Raya <nathan.setyawan96@gmail.com>
2022-07-06 18:05:57 +0800

    style: change all icons to svg

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,21 +1,21 @@
-import { Flex, Text, Icon, useColorModeValue } from "@chakra-ui/react";
+import { Box, Icon, Tooltip } from "@chakra-ui/react";
 
-// pass string and react-icons
-export default function Skill({ tool, icon }: { tool: string, icon: any }) {
-    const bgColor = "#fdd2d8"
+// pass string and <svg>
+export default function Skill({ name, icon }: { name: string, icon: JSX.Element }) {
     return (
-        <Flex mb={3}>
-            <Flex
+        <Tooltip label={name}>
+            <Box
                 p={2}
                 w="max-content"
                 h="max-content"
-                bg={bgColor}
+                bgColor="white"
                 rounded="md"
                 mr={3}
             >
-                <Icon color="gray.700" w={8} h={8} as={icon} />
-            </Flex>
-            <Text my="auto">{tool}</Text>
-        </Flex>
+                <Icon color="gray.700" w={12} h={12}>
+                    {icon}
+                </Icon>
+            </Box>
+        </Tooltip>
     )
 }
--- components/Skillset.tsx
+++ components/Skillset.tsx
@@ -1,8 +1,21 @@
-import { Heading, Stack, UnorderedList } from "@chakra-ui/react";
+import { Flex, Heading, Stack, Tooltip, Box, Icon } from "@chakra-ui/react";
 import Skill from './Skill';
-import { FaReact } from 'react-icons/fa';
-import { BsQuestion } from 'react-icons/bs';
-import { SiGatsby, SiNextdotjs, SiChakraui, SiTypescript, SiPython, SiGit, SiDocker, SiKubernetes, SiLinux, SiGraphql, SiMongodb, SiPostgresql, SiCypress } from "react-icons/si";
+import PostgreSQL from "../icons/postgresql";
+import MongoDB from "../icons/mongodb";
+import GraphQL from "../icons/graphql";
+import Go from "../icons/go";
+import Python from "../icons/python";
+import Typescript from "../icons/typescript";
+import Javascript from "../icons/javascript";
+import React from "../icons/react";
+import Linux from "../icons/linux";
+import Kubernetes from "../icons/kubernetes";
+import Docker from "../icons/docker";
+import Git from "../icons/git";
+import Next from "../icons/next";
+import Gatsby from "../icons/gatsby";
+import Cypress from "../icons/cypress";
+import ChakraUI from "../icons/chakraui";
 
 export default function Skillset(): JSX.Element {
     return (
@@ -15,47 +28,48 @@ export default function Skillset(): JSX.Element {
                 <Heading as="h2" size="md">
                     Programming Languages
                 </Heading>
-                <UnorderedList mt={3} mb={5}>
-                    <Skill tool="Go" icon={BsQuestion} />
-                    <Skill tool="Python" icon={SiPython} />
-                    <Skill tool="Typescript / Javascript" icon={SiTypescript} />
-                </UnorderedList>
+                <Flex mt={3} mb={5}>
+                    <Skill name="Go" icon={Go} />
+                    <Skill name="Python" icon={Python} />
+                    <Skill name="Javascript" icon={Javascript} />
+                    <Skill name="Typescript" icon={Typescript} />
+                </Flex>
             </Stack>
 
             <Stack mt={5}>
                 <Heading as="h2" size="md">
                     Web technologies
                 </Heading>
-                <UnorderedList mt={3} mb={5}>
-                    <Skill tool="React" icon={FaReact} />
-                    <Skill tool="Next.js" icon={SiNextdotjs} />
-                    <Skill tool="Gatsby.js" icon={SiGatsby} />
-                    <Skill tool="ChakraUI" icon={SiChakraui} />
-                    <Skill tool="Cypress.js" icon={SiCypress} />
-                </UnorderedList>
+                <Flex mt={3} mb={5}>
+                    <Skill name="React" icon={React} />
+                    <Skill name="Next.js" icon={Next} />
+                    <Skill name="Gatsby.js" icon={Gatsby} />
+                    <Skill name="ChakraUI" icon={ChakraUI} />
+                    <Skill name="Cypress" icon={Cypress} />
+                </Flex>
             </Stack>
 
             <Stack mt={5}>
                 <Heading as="h2" size="md">
                     Tools
                 </Heading>
-                <UnorderedList mt={3} mb={5}>
-                    <Skill tool="Git" icon={SiGit} />
-                    <Skill tool="Docker" icon={SiDocker} />
-                    <Skill tool="Kubernetes" icon={SiKubernetes} />
-                    <Skill tool="Linux / Unix" icon={SiLinux} />
-                </UnorderedList>
+                <Flex mt={3} mb={5}>
+                    <Skill name="Git" icon={Git} />
+                    <Skill name="Docker" icon={Docker} />
+                    <Skill name="Kubernetes" icon={Kubernetes} />
+                    <Skill name="Linux" icon={Linux} />
+                </Flex>
             </Stack>
 
             <Stack mt={5}>
                 <Heading as="h2" size="md" >
                     Database framework
                 </Heading>
-                <UnorderedList mt={3}>
-                    <Skill tool="GraphQL" icon={SiGraphql} />
-                    <Skill tool="MongoDB" icon={SiMongodb} />
-                    <Skill tool="PostgresQL" icon={SiPostgresql} />
-                </UnorderedList>
+                <Flex mt={3}>
+                    <Skill name="GraphQL" icon={GraphQL} />
+                    <Skill name="MongoDB" icon={MongoDB} />
+                    <Skill name="PostgresQL" icon={PostgreSQL} />
+                </Flex>
             </Stack>
         </>
     )
