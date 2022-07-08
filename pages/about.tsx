e8229cab4048cb8974a8df05ebede1af3dcca68c
Bijon Setyawan Raya <nathan.setyawan96@gmail.com>
2022-07-09 00:30:29 +0800

    chore: put resume link

--- pages/about.tsx
+++ pages/about.tsx
@@ -4,6 +4,7 @@ import Skillset from '../components/Skillset'
 import {  Heading, Link, Stack, Text } from '@chakra-ui/react'
 
 export default function About(): JSX.Element {
+    const resumeLink = "https://drive.google.com/file/d/1n2HO7mkmi0_TTvP5SyOY4Dj2nKUrKb80/view?usp=sharing"
     return (
         <Layout>
             <Stack>
@@ -20,7 +21,7 @@ export default function About(): JSX.Element {
             <Skillset />
 
             <Text mt={10} mb={5}>
-                For more details, my resume can be downloaded <Link href={process.env.RESUME_URL} color="#fc909f" isExternal>here</Link>.
+                For more details, my resume can be downloaded <Link href={resumeLink} color="#fc909f" isExternal>here</Link>.
             </Text>
         </Layout>
     )
