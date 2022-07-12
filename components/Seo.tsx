1360cde4b711b14ad8a1b10cc45ec03faa3045b9
Bijon Setyawan Raya <nathan.setyawan96@gmail.com>
2022-07-12 19:01:23 +0800

    chore: fix SEO

--- components/Seo.tsx
+++ components/Seo.tsx
@@ -11,35 +11,34 @@ export default function Seo({ title, publish, description, type }: { title: stri
     return (
         <Head>
             <title>{title} - {meta.author}</title>
+            <meta name="title" content={title} />
             <meta name="viewport" content="width=device-width" />
-            <meta name="description" content={meta.description} />
             <meta name="author" content={meta.author} />
-            <meta name="robots" content="follow, index" />
             <link rel="canonical" href={`https://bsraya.com${router.asPath}`} />
-            <link rel="icon" href="https://www.bsraya.com/images/avatar.png" />
-            <meta property="og:url" content={`https://bsraya.com${router.asPath}`} />
+            <link rel="icon" href={meta.image} />
+
             <meta property="og:type" content={type} />
-            <meta property="og:site_name" content={meta.author} />
+            <meta property="og:url" content={`https://bsraya.com${router.asPath}`} />
             <meta property="og:title" content={title + " - " + meta.author} />
             <meta property="og:image" content={meta.image} />
-            <meta name="twitter:site" content="@BijonRaya" />
-            <meta name="twitter:creator" content={meta.author} />
-            <meta name="twitter:card" content="summary_large_image" />
-            <meta name="twitter:title" content={title + " - " + meta.author} />
-            <meta name="twitter:image" content={meta.image} />
+
+            <meta property="twitter:card" content="summary_large_image" />
+            <meta property="twitter:url" content="@BijonRaya" />
+            <meta property="twitter:creator" content={`https://bsraya.com${router.asPath}`} />
+            <meta property="twitter:image" content={meta.image} />
             {
                 !description ? (
                     <>
-                        <meta name="description" content={meta.description}  /> 
+                        <meta name="description" content={meta.description} />
                         <meta property="og:description" content={meta.description} />
-                        <meta name="twitter:description" content={meta.description} />
+                        <meta property="twitter:description" content={meta.description} />
                     </>
                 )
                 : (
                     <>
                         <meta name="description" content={description} />
                         <meta property="og:description" content={description} />
-                        <meta name="twitter:description" content={description} />
+                        <meta property="twitter:description" content={description} />
                     </>
                 )
             }
