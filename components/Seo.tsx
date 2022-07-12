60a37aed6188af764b1e4d68aa0c507364e257d3
Bijon Setyawan Raya <nathan.setyawan96@gmail.com>
2022-07-12 19:11:32 +0800

    chore: allow robot to crawl

--- components/Seo.tsx
+++ components/Seo.tsx
@@ -10,41 +10,42 @@ export default function Seo({ title, publish, description, type }: { title: stri
     }
     return (
         <Head>
-            <title>{title} - {meta.author}</title>
-            <meta name="title" content={title} />
+            <title>{ title } - { meta.author }</title>
+            <meta name="title" content={ title } />
+            <meta name="robots" content="follow, index" />
             <meta name="viewport" content="width=device-width" />
-            <meta name="author" content={meta.author} />
-            <link rel="canonical" href={`https://bsraya.com${router.asPath}`} />
-            <link rel="icon" href={meta.image} />
+            <meta name="author" content={ meta.author } />
+            <link rel="canonical" href={ `https://bsraya.com${router.asPath}` } />
+            <link rel="icon" href={ meta.image } />
 
-            <meta property="og:type" content={type} />
-            <meta property="og:url" content={`https://bsraya.com${router.asPath}`} />
-            <meta property="og:title" content={title + " - " + meta.author} />
-            <meta property="og:image" content={meta.image} />
+            <meta property="og:type" content={ type } />
+            <meta property="og:url" content={ `https://bsraya.com${router.asPath}` } />
+            <meta property="og:title" content={ title + " - " + meta.author } />
+            <meta property="og:image" content={ meta.image } />
 
             <meta property="twitter:card" content="summary_large_image" />
             <meta property="twitter:url" content="@BijonRaya" />
-            <meta property="twitter:creator" content={`https://bsraya.com${router.asPath}`} />
-            <meta property="twitter:image" content={meta.image} />
+            <meta property="twitter:creator" content={ `https://bsraya.com${router.asPath}` } />
+            <meta property="twitter:image" content={ meta.image } />
             {
                 !description ? (
                     <>
-                        <meta name="description" content={meta.description} />
-                        <meta property="og:description" content={meta.description} />
-                        <meta property="twitter:description" content={meta.description} />
+                        <meta name="description" content={ meta.description } />
+                        <meta property="og:description" content={ meta.description } />
+                        <meta property="twitter:description" content={ meta.description } />
                     </>
                 )
                 : (
                     <>
-                        <meta name="description" content={description} />
-                        <meta property="og:description" content={description} />
-                        <meta property="twitter:description" content={description} />
+                        <meta name="description" content={ description } />
+                        <meta property="og:description" content={ description } />
+                        <meta property="twitter:description" content={ description } />
                     </>
                 )
             }
             {
                 publish && (
-                    <meta property="article:published_time" content={publish} />
+                    <meta property="article:published_time" content={ publish } />
                 )
             }
         </Head>
