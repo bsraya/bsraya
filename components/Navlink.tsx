a2903fb9ae08b24923dcfb0070ea6c73596c48e6
nathansetyawan96 <nathan.setyawan96@gmail.com>
2022-07-18 14:43:27 +0800

    style: decorate active links

--- components/Navlink.tsx
+++ components/Navlink.tsx
@@ -1,19 +1,31 @@
-import { Link } from '@chakra-ui/react'
+import { Link, useColorModeValue } from '@chakra-ui/react'
 import NextLink from 'next/link'
+import { useRouter } from 'next/router'
 
 export default function Navlink ({ href, children }: { href: string, children: React.ReactNode }): JSX.Element {
+    const router = useRouter()
+    const fontColor = useColorModeValue('black', 'white')
+    var isActive = router.pathname === href
+
+    if (href.includes("/blog")) {
+        isActive = router.pathname.includes("/blog")
+    }
+
+    if (href.includes("/portfolio")) {
+        isActive = router.pathname.includes("/portfolio")
+    }
+
     return (
+        // if href is equal to current path, then it will be active
         <NextLink href={href}>
             <Link
                 mr={5}
-                rounded={'md'}
                 variant="ghost"
-                _hover={{
-                    textDecoration: 'underline',
-                }}
-            >
-                {children}
-            </Link>
+                fontWeight="bold"
+                color={isActive ? fontColor : 'gray.500'}
+                _hover={{ color: fontColor }}
+                _active={{ color: fontColor }}
+            >{children}</Link>
         </NextLink>
     )
 }
