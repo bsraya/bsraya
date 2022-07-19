027defbb74e6696658980414bdbb4c4a48518512
Bijon Setyawan Raya <nathan.setyawan96@gmail.com>
2022-07-19 12:00:59 +0800

    add: detect theme from cookies

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,31 @@
 import 'katex/dist/katex.min.css'
 import theme from '../styles/theme'
 import type { AppProps } from 'next/app'
-import { ChakraProvider } from '@chakra-ui/react'
+import {
+    ChakraProvider,
+    cookieStorageManagerSSR,
+    localStorageManager,
+} from '@chakra-ui/react'
+import { NextApiRequest } from 'next'
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
     return (
-        <ChakraProvider theme={theme}>
+        <ChakraProvider
+            theme={theme}
+            colorModeManager={
+                typeof pageProps.cookies === 'string'
+                ? cookieStorageManagerSSR(pageProps.cookies)
+                : localStorageManager
+            }
+        >
             <Component {...pageProps} />
         </ChakraProvider>
     )
+}
+
+export const getInitialProps = (req: NextApiRequest) => {
+    const cookies = req.headers.cookie
+    return {
+        cookies,
+    }
 }
