d7e4aee3d0dc7e15648b3b3d1f83e4f2e045418e
Bijon Setyawan Raya <johnbjohn@Bijons-MBP.hitronhub.home>
2022-10-18 00:09:31 +0800

    update dependencies

--- next.config.js
+++ next.config.js
@@ -17,13 +17,12 @@ const nextConfig = {
 const withPlugins = require('next-compose-plugins')
 const runtimeCaching = require('next-pwa/cache')
 const withPWA = require('next-pwa')({
-    pwa: {
-        dest: "public",
-        register: true,
-        skipWaiting: true,
-        disable: process.env.NODE_ENV === 'development',
-        runtimeCaching,
-    }
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development',
+    runtimeCaching,
+
 })
 const withImages = require('next-images')
 
