0d68fbddc4fb2a7fc0b43b7504cbae0478adc622
Bijon Setyawan Raya <nathan.setyawan96@gmail.com>
2022-11-03 15:41:29 +0800

    chore: include open sans and pt serif

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -19,19 +19,14 @@ export default class MyDocument extends NextDocument {
                         crossOrigin="true"
                     />
                     <link
-                        as="font"
+                        href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
                         rel="stylesheet"
-                        crossOrigin="anonymous"
-                        href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
                     />
                     <link
-                        as="font"
-                        rel="stylesheet"
-                        crossOrigin="anonymous"
-                        href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap"
-                    />
+                        href="https://fonts.googleapis.com/css2?family=PT+Serif:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+                        rel="stylesheet">
+                    </link>
                     <link
-                        as="font"
                         rel="stylesheet"
                         href="https://fonts.googleapis.com/css2?family=Fira+Code&display=swap"
                     />
