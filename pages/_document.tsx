eb652fb9c1fc13b35aaf64a2ca20fbd3ba71a0c6
Bijon Setyawan Raya <nathan.setyawan96@gmail.com>
2022-11-06 21:47:20 +0800

    chore: remove open sans

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -19,16 +19,12 @@ export default class MyDocument extends NextDocument {
                         crossOrigin="true"
                     />
                     <link
-                        href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
+                        href="https://fonts.googleapis.com/css2?family=PT+Serif:ital,wght@0,400;0,700;1,400;1,700&display=swap"
                         rel="stylesheet"
                     />
                     <link
-                        href="https://fonts.googleapis.com/css2?family=PT+Serif:ital,wght@0,400;0,700;1,400;1,700&display=swap"
-                        rel="stylesheet">
-                    </link>
-                    <link
+                        href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400;500;600;700&display=swap"
                         rel="stylesheet"
-                        href="https://fonts.googleapis.com/css2?family=Fira+Code&display=swap"
                     />
                     <link
                         rel="stylesheet"
