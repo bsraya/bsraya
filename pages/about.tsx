fda3bc8afe972f038c00330cd6f10dc8be9ca8d7
Bijon Setyawan Raya <nathan.setyawan96@gmail.com>
2022-11-15 11:00:34 +0800

    style: reduce line height

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,4 +1,4 @@
-import { Container, Heading, Link, Stack, Text } from '@chakra-ui/react'
+import { Box, Container, Heading, Link, Stack, Text } from '@chakra-ui/react'
 import { ExternalLinkIcon } from '@chakra-ui/icons'
 
 import Seo from '../components/Seo'
@@ -12,24 +12,28 @@ export default function About(): JSX.Element {
     return (
         <Layout>
             <Seo title="About" type="website" />
-            <Stack>
-                <Heading as="h1">
-                    About me
-                </Heading>
-                <Text lineHeight="taller" color="gray.500">
-                    Hey, I am Bijon Setyawan Raya. I am a fullstack developer, and also a Computer Science graduate student at National Tsing Hua University in Hsinchu, Taiwan.
-                </Text>
-                <Text lineHeight="taller" color="gray.500">
-                    In my graduate program, I am focusing on Deep Learning. Due to the necessity of my lab, I created a distributed deep learning system that can be used to train deep learning models efficiently on a cluster of machines.
-                    I also created a web application that can be used to monitor the status of the trainig jobs, as well as the machines in the cluster.
-                </Text>
-                <Text lineHeight="taller" color="gray.500">
-                    Down there, you will be able to see my experience as a developer, as well as the technologies that I am and have used in the past. Those technologies are shown as icons, and please hover over the icons to see its description.
-                    For more details, my resume can be seen or downloaded <><Link href={resumeLink} color="dark" _dark={{ color: "light" }} isExternal textDecoration="underline" _hover={{ textDecoration: "none" }}>here</Link><ExternalLinkIcon mx="5px" mb="4px" /></>.
-                </Text>
-            </Stack>
-            <Experience />
-            <Skillset />
+            <Box maxWidth="720px" mx={5}>
+                <Stack>
+                    <Heading as="h1">
+                        About me
+                    </Heading>
+                    <Text color="gray.500">
+                        Hey, I am Bijon Setyawan Raya. I am a fullstack developer, and also a Computer Science graduate student at National Tsing Hua University in Hsinchu, Taiwan.
+                    </Text>
+                    <Text color="gray.500">
+                        In my graduate program, I am focusing on Deep Learning. Due to the necessity of my lab, I created a distributed deep learning system that can be used to train deep learning models efficiently on a cluster of machines.
+                        I also created a web application that can be used to monitor the status of the trainig jobs, as well as the machines in the cluster.
+                    </Text>
+                    <Text color="gray.500">
+                        Down there, you will be able to see my experience as a developer, as well as the technologies that I am and have used in the past. Those technologies are shown as icons, and please hover over the icons to see its description.
+                        For more details, my resume can be seen or downloaded <><Link href={resumeLink} color="dark" _dark={{ color: "light" }} isExternal textDecoration="underline" _hover={{ textDecoration: "none" }}>here</Link><ExternalLinkIcon mx="5px" mb="4px" /></>.
+                    </Text>
+                </Stack>
+            </Box>
+            <Box mx={5}>
+                <Experience />
+                <Skillset />
+            </Box>
         </Layout>
     )
 }
