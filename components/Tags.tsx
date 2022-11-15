42fd2174f10796e91b00d814c1311507f7235ab7
Bijon Setyawan Raya <nathan.setyawan96@gmail.com>
2022-11-15 11:34:25 +0800

    fix: key issues

--- components/Tags.tsx
+++ components/Tags.tsx
@@ -7,30 +7,29 @@ export default function Tags({ tags }: { tags: string[] }) {
             {
                 tags.map((tag: string, index: any) => {
                     return (
-                        
-                            <Button
-                                mr={2}
-                                variant="ghost"
-                                px={3}    
-                                _dark={{
-                                    color: "gray.100",
-                                    bg: 'gray.600',
-                                    '&:hover': {
-                                        bg: 'gray.500'
-                                    },
-                                    boxShadow: '5px 5px 0px rgba(255, 255, 255, 0.1)'
-                                }}
-                                bg="white"
-                                _hover={{
-                                    bgColor: 'gray.50'
-                                }}
-                                color="gray.500"
-                                fontFamily="Fira Code"
-                                boxShadow="5px 5px 0px rgba(0, 0, 0, 0.1)"
+                        <Button
+                            key={index}
+                            mr={2}
+                            variant="ghost"
+                            px={3}    
+                            _dark={{
+                                color: "gray.100",
+                                bg: 'gray.600',
+                                '&:hover': {
+                                    bg: 'gray.500'
+                                },
+                                boxShadow: '5px 5px 0px rgba(255, 255, 255, 0.1)'
+                            }}
+                            bg="white"
+                            _hover={{
+                                bgColor: 'gray.50'
+                            }}
+                            color="gray.500"
+                            fontFamily="Fira Code"
+                            boxShadow="5px 5px 0px rgba(0, 0, 0, 0.1)"
                         >
                             <NextLink
                                 passHref
-                                key={index}
                                 href={'/tag/' + tag}
                             >
                                 {tag}
