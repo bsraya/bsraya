dc36f398c29ff11d8b665ff31570ce9e1aa39e7b
Bijon Setyawan Raya <nathan.setyawan96@gmail.com>
2022-11-24 19:56:57 +0800

    feature: add animation

--- components/Cards/Tag.tsx
+++ components/Cards/Tag.tsx
@@ -10,12 +10,18 @@ export default function Tag({ tag }: { tag: string }) {
       variant="ghost"
       px={3}
       bg="white"
-      _hover={{
-          bgColor: 'gray.50'
-      }}
       color="gray.500"
       fontFamily="Fira Code"
+      // set border color
+      border="1px solid"
+      borderColor="gray.200"
       boxShadow="5px 5px 0px rgba(0, 0, 0, 0.1)"
+      transform='translateY(5px)'
+      transition="box-shadow .15s ease-in-out, transform .15s ease-in-out"
+      _hover={{
+        bgColor: 'gray.50',
+        transform: 'translateY(0)'
+      }}
     >
       <NextLink
           passHref
