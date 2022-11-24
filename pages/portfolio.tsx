593abce726a2007eb63fb88a35306b36d803a71c
nathansetyawan96 <nathan.setyawan96@gmail.com>
2022-11-24 22:16:09 +0800

    fix: post interface name

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -4,11 +4,11 @@ import matter from 'gray-matter'
 import Layout from '../components/Layout'
 import Posts from './../components/Posts'
 import { Heading } from '@chakra-ui/react'
-import type { Post } from '../lib/types'
+import type { IPost } from '../lib/types'
 import { sortPosts } from '../lib/posts'
 import Seo from '../components/Seo'
 
-export default function Portfolio({ portfolios }: { portfolios: Post[] }): JSX.Element {
+export default function Portfolio({ portfolios }: { portfolios: IPost[] }): JSX.Element {
     return (
         <Layout>
             <Seo title="Portfolio" type="website" />
@@ -23,7 +23,7 @@ export const getStaticProps = async () => {
     const folders = fs.readdirSync(path.join(process.cwd(), 'content', 'portfolios'))
     
     // iterate through all the folders
-    var portfolios: Post[] = folders.map(slug => {
+    var portfolios: IPost[] = folders.map(slug => {
         const content = fs.readFileSync(path.join('content', 'portfolios', slug, 'index.mdx'), 'utf-8')
         const { data: frontMatter } = matter(content)
         return {
@@ -32,7 +32,7 @@ export const getStaticProps = async () => {
         }
     })
     
-    portfolios = portfolios.filter((portfolio: Post) => portfolio.frontMatter.publish)
+    portfolios = portfolios.filter((portfolio: IPost) => portfolio.frontMatter.publish)
 
     portfolios = sortPosts(portfolios)
 
