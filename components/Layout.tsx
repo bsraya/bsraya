0e6c9fb37b7e2fb4f1fe23df4555d7c8c462ccb9
Bijon Setyawan Raya <nathan.setyawan96@gmail.com>
2022-12-01 18:37:36 +0800

    feat: scrollbar progress

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,23 +5,47 @@ import prismStyles from '../styles/prism'
 import katexStyles from '../styles/katex'
 import { Global } from '@emotion/react';
 import BackToTop from './Navigation/BackToTop'
+import { motion, useScroll, useSpring } from "framer-motion";
 
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+    const { scrollYProgress } = useScroll();
+    const scaleX = useSpring(scrollYProgress, {
+        stiffness: 100,
+        damping: 30,
+        restDelta: 0.001
+    })
+    
     return (
-        <Container
-            as="main"
-            my={10}
-            maxWidth="container.md"
-        >
-            <Navbar />
-            <Global
-                styles={[prismStyles, katexStyles]}
+        <>
+            <motion.div
+                style={{
+                    scaleX: scaleX,
+                    height: "5px",
+                    background: "#2b6cb0",
+                    position: "fixed",
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    transformOrigin: "0%",
+                    zIndex: 999
+                }}
             />
-            {children}
-            <Footer />
-            <BackToTop />
-        </Container>
+            <Container
+                as="main"
+                my={10}
+                maxWidth="container.md"
+            >
+                
+                <Navbar />
+                <Global
+                    styles={[prismStyles, katexStyles]}
+                />
+                {children}
+                <Footer />
+                <BackToTop />
+            </Container>
+        </>
     );
 }
 
