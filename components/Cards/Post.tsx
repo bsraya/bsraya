15b0893ae4b4b234979061c49b478961f8b49233
Bijon Setyawan Raya <nathan.setyawan96@gmail.com>
2022-12-23 01:12:49 +0800

    feat: card image

--- components/Cards/Post.tsx
+++ components/Cards/Post.tsx
@@ -32,7 +32,7 @@ export default function Post({ post, type }: { post: IPost; type: string }) {
                     display="flex"
                     borderTopRadius="md"
                     backgroundSize="cover"
-                    backgroundImage={`url(https://images.unsplash.com/photo-1667513993054-9335374c6b94?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80)`}
+                    backgroundImage={`url(${post.frontMatter.img})`}
                 >
                     <Heading
                         as="h2"
