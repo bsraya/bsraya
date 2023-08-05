2ebb56255740fd5991bb8e470e84572bf76e6814
Bijon Setyawan Raya <nathan.setyawan96@gmail.com>
2023-08-05 22:47:04 +0800

    feat: add analytics feature

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,13 @@ import 'katex/dist/katex.min.css'
 import theme from '../styles/theme'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
+import { Analytics } from '@vercel/analytics/react'
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
     return (
         <ChakraProvider theme={theme}>
             <Component {...pageProps} />
+            <Analytics />
         </ChakraProvider>
     )
 }
