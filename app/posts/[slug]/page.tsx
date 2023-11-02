39e71f68e9702fbca9c945b1d37dfda76ea918c1
Bijon Setyawan Raya <nathan.setyawan96@gmail.com>
2023-11-03 01:23:41 +0800

    feat: redesign post page

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,8 +1,8 @@
-import { allPosts } from '../../../.contentlayer/generated'
-import { notFound } from 'next/navigation'
-import Mdx from "../../../components/mdx-components"
 import Link from 'next/link';
 import { format } from 'date-fns';
+import { notFound } from 'next/navigation'
+import Mdx from "../../../components/mdx-components"
+import { allPosts } from '../../../.contentlayer/generated'
 
 interface PostProps {
     params: {
@@ -21,36 +21,41 @@ async function getPostsFromParams(slug: string) {
 export default async function Post({ params }: PostProps) {
     const post = await getPostsFromParams(params.slug)
     return (
-        <div className="p-10">
+        <div className="lg:p-10 p-5">
             <div className="text-xl mb-36"><Link href="/" className='no-underline'>Bijon Setyawan Raya</Link> / <Link href="/posts" className="font-bold no-underline">Posts</Link></div>
             
-            <Link href="/posts">
-                <div className="flex w-fit px-5 py-2 border rounded-full hover:border-gray-500 cursor-pointer">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 mr-2">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
-                    </svg>
-                    <p>Posts</p>
-                </div>
-            </Link>
-            <h1 className="text-5xl font-thin font-serif my-5">{post.title}</h1>
-            <div className="flex border-y-2 py-2">
-                <div className="text-xl">{post.description}</div>
+            <div className='flex items-center text-xl gap-3'>
+                <Link href="/posts">
+                    <div className="flex w-fit px-5 py-2 border rounded-full hover:border-gray-500 cursor-pointer">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 mr-2">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
+                        </svg>
+                        <p>Posts</p>
+                    </div>
+                </Link>
                 {
-                    post.date && (
-                        <div className="text-xl ml-auto">{format(new Date(post.date), 'dd MMMM yyyy')}</div>
+                    post.tag && (
+                        <Link href={`/tags/${post.tagAsParams}`}>
+                            <div className="hover:underline cursor-pointer">
+                                <p>{post.tag}</p>
+                            </div>
+                        </Link>
                     )
                 }
             </div>
-            {
-                post.tags && (
-                    <div className="my-5 text-lg">
-                        {post.tags.map((tag) => (
-                            <span key={tag} className="mr-2">#{tag}</span>
-                        ))}
-                    </div>
-                )
-            }
-            <div className="prose lg:prose-xl">
+            
+            <h1 className="xl:text-5xl text-4xl font-thin font-serif my-5">{post.title}</h1>
+            <div className="text-lg sm:text-md w-full border-y-2 py-3 text-gray-500 flex flex-col lg:flex-row lg:justify-between gap-3">
+                <blockquote className="italic">
+                    &quot;{post.description}&quot;
+                </blockquote>
+                {
+                    post.date && (
+                        <div>{format(new Date(post.date), 'dd MMMM yyyy')}</div>
+                    )
+                }
+            </div>
+            <div className="prose lg:prose-xl max-w-[120ch] my-32">
                 <Mdx code={post.body.code} />
             </div>
         </div>
