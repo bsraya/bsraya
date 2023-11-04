7ccff880e707eae4f45d1cbac1f7262cf9f18a52
nathansetyawan96 <nathan.setyawan96@gmail.com>
2023-11-05 00:44:28 +0800

    fix: rename params

--- app/tags/[slug]/page.tsx
+++
@@ -1,9 +0,0 @@
-import Link from 'next/link';
-
-export default function Tags() {
-  return (
-    <div className="p-10">
-      <div className='text-xl mb-36'><Link href='/'>Bijon Setyawan Raya</Link> / <Link href='/tags' className='font-bold'>Tags</Link></div>
-    </div>
-  )
-}
