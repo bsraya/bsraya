9c4ace49162645568b5c969cf9fe2e60c8230c4e
nathansetyawan96 <nathan.setyawan96@gmail.com>
2023-11-05 00:43:40 +0800

    style: responsive layout padding

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function About() {
     return (
-        <div className="p-10">
+        <div className="lg:p-10 p-5">
             <div className="text-xl mb-36"><Link href="/">Bijon Setyawan Raya</Link> / <Link href="/posts" className="font-bold">About Me</Link></div>
         </div>
     )
--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -7,8 +7,9 @@ export default function Posts() {
     const posts = allPosts.filter((post) => post.published === true);
 
     return (
-        <div className="p-10">
+        <div className="lg:p-10 p-5">
             <div className="text-xl mb-36"><Link href="/">Bijon Setyawan Raya</Link> / <Link href="/posts" className="font-bold">Posts</Link></div>
+            
             <h1 className="underline mb-5">All Posts</h1>
             <div className="flex flex-col gap-10 mb-32 w-3/4 md:w-full">
                 {
@@ -16,24 +17,23 @@ export default function Posts() {
                         return (
                         <Link key={slugAsParams} href={`/posts/${slugAsParams}`} as={`/posts/${slugAsParams}`}>
                             <div className="h-30 xl:flex block gap-5">
-                            <Image
-                                src="https://dummyimage.com/200x125.png/a9a9a9/fff"
-                                alt="dummy"
-                                className="rounded-lg col-span-1"
-                                width={200}
-                                height={125}
-                            />
-                            <div className="col-span-auto mt-3">
-                                <h1 className="text-2xl font-bold">{title}</h1>
-                                <p className="text-lg my-2">{description}</p>
-                                <div className="text-xl ml-auto">{format(new Date(date), 'dd MMMM yyyy')}</div>
-                            </div>
+                                <Image
+                                    src="https://dummyimage.com/200x125.png/a9a9a9/fff"
+                                    alt="dummy"
+                                    className="rounded-lg col-span-1"
+                                    width={200}
+                                    height={125}
+                                />
+                                <div className="col-span-auto mt-3">
+                                    <h1 className="text-2xl font-bold">{title}</h1>
+                                    <p className="text-lg my-2">{description}</p>
+                                    <div className="text-xl ml-auto">{format(new Date(date), 'dd MMMM yyyy')}</div>
+                                </div>
                             </div>
                         </Link>
                         )
                     })
                 }
-                {/* <Link to="#" className="py-2 px-5 text-xl w-fit rounded-full border border-2 hover:border-gray-500">Load More</Link> */}
             </div>
         </div>
     )
