df43b324a40350f913035958de2909232592d630
nathansetyawan96 <nathan.setyawan96@gmail.com>
2023-11-04 18:28:19 +0800

    style: align icon and word

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -26,7 +26,7 @@ export default async function Post({ params }: PostProps) {
             
             <div className='flex items-center text-xl gap-3'>
                 <Link href="/posts">
-                    <div className="flex w-fit px-5 py-2 border rounded-full hover:border-gray-500 cursor-pointer">
+                    <div className="flex w-fit px-5 py-2 border rounded-full hover:border-gray-500 cursor-pointer  items-center">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 mr-2">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
                         </svg>
