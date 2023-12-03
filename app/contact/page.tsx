da2bf3ba75afaa0ca5794002ca95ed0818acd4e5
Bijon Setyawan Raya <nathan.setyawan96@gmail.com>
2023-12-03 15:56:36 +0800

    chore: remove contact page

--- app/contact/page.tsx
+++
@@ -1,9 +0,0 @@
-import Link from 'next/link'
-
-export default function ContactMe() {
-    return (
-        <div className="lg:p-10 p-5">
-            <div className="text-xl mb-36"><Link href="/">Bijon Setyawan Raya</Link> / <Link href="/posts" className="font-bold">Contact Me</Link></div>
-        </div>
-    )
-}
