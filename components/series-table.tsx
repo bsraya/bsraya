9f4705277dabf15c1ffa8439291ead1a39d97ddf
Bijon Setyawan Raya <nathan.setyawan96@gmail.com>
2023-12-11 23:45:39 +0800

    style: change font

--- components/series-table.tsx
+++ components/series-table.tsx
@@ -8,13 +8,13 @@ interface SeriesPost {
 export default function SeriesTable({ title, series }: { title: string, series: SeriesPost[] }) {
   return (
     <div className="mb-32 p-5 rounded-lg border not-prose w-2/3 text-gray-800">
-      <p className="text-2xl mb-5 underline">{title}: {series.length} Parts</p>
-      <ol className="list-decimal list-inside flex flex-col gap-3 ml-3">
+      <p className="font-bebasneue text-2xl mb-5 underline">{title}: {series.length} Parts</p>
+      <ol className="font-baskervville list-decimal list-inside flex flex-col gap-3 ml-3">
         {
           series.map(({ title, current }: SeriesPost) => {
             const slug = title.toLowerCase().replace(/ /g, '-');
             return (
-              <li key={slug} className={`hover:font-bold ${current ? 'font-bold' : ''}`}>
+              <li key={slug} className={`hover:underline ${current ? 'font-bold' : ''}`}>
                 <Link href={`/posts/${slug}`} as={`/posts/${slug}`}>
                   {title}
                 </Link>
