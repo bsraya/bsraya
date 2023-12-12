b83b1fec536cbd5ed18c5a7cfc7d73ab71c56d17
Bijon Setyawan Raya <nathan.setyawan96@gmail.com>
2023-12-13 00:09:52 +0800

    fix: add the experience page back

--- components/side-menu.tsx
+++ components/side-menu.tsx
@@ -6,6 +6,7 @@ export default function SideMenu() {
             <p className="font-novasquare text-4xl">Bijon<br />Setyawan<br />Raya</p>
             <ul className="font-baskervville mt-auto text-xl">
                 <li className="hover:underline w-fit"><Link href="/about">About</Link></li>
+                <li className="hover:underline w-fit"><Link href="/experience">Experience</Link></li>
                 <li className="hover:underline w-fit"><Link href="/posts">Posts</Link></li>
             </ul>
             <Link href="/works">
