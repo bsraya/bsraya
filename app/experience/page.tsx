894ee22755fdd908812d7c9712c695f97f2f610b
Bijon Setyawan Raya <nathan.setyawan96@gmail.com>
2023-12-13 10:16:46 +0800

    style: increase font size

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -13,11 +13,13 @@ export default function Experience() {
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 mr-2">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
                         </svg>
-                        <p className="font-baskervville">Home</p>
+                        <p className="font-baskervville text-lg">Home</p>
                     </div>
                 </Link>
             </div>
 
+            <p className='font-baskervville mb-10 text-lg'>Need something on the go? Worry not, you can download my <ExternalLink text="resume" url="https://drive.google.com/file/d/1jC-51cyHc-xOxdgyzh787VEHsWKMdQM7/view?usp=sharing" /> here.</p>
+            
             <div className="flex flex-col gap-20 mb-10">
                 <div>
                     <h1 className="font-bebasneue mb-10 text-5xl">Experience</h1>
--- app/works/page.tsx
+++ app/works/page.tsx
@@ -20,7 +20,7 @@ export default function Works() {
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 mr-2">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
                         </svg>
-                        <p className="font-baskervville">Home</p>
+                        <p className="font-baskervville text-lg">Home</p>
                     </div>
                 </Link>
             </div>
