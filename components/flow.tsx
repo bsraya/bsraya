4cae9931d9433dbcf02a088aaa9bb7225288c40e
Bijon Setyawan Raya <nathan.setyawan96@gmail.com>
2024-01-19 16:44:27 +0800

    style: support mobile version

--- components/flow.tsx
+++ components/flow.tsx
@@ -46,7 +46,7 @@ export default function Flow({
   );
 
   return (
-    <div style={{ width: '100vh', height: '500px', border: '1px solid #eee' }}>
+    <div style={{ width: '100%', height: '500px', border: '1px solid #eee' }}>
       <ReactFlow
         nodes={nodes}
         edges={edges}
