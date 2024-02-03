4112d753fd24d93828b795ecdb99ef1d3707d533
Bijon Setyawan Raya <nathan.setyawan96@gmail.com>
2024-02-04 00:28:51 +0800

    fix: prevent video being downloaded on pwa

--- components/video.tsx
+++ components/video.tsx
@@ -1,10 +1,10 @@
 export default function Video({ url, title }: { url: string, title: string }) {
     return (
-        <div className="aspect-w-16 aspect-h-9">
-            <iframe
+        <div className="aspect-w-16 aspect-h-9 mb-20">
+            <video
                 src={url}
                 title={title}
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                controls
             />
         </div>
     )
