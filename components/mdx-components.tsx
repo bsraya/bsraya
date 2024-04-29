7d1326c5e9cb927a67157a2ad1adbc2b456651e1
Bijon Setyawan Raya <nathan.setyawan96@gmail.com>
2024-04-29 10:49:12 +0800

    style: set height and width, instead of class

--- components/mdx-components.tsx
+++ components/mdx-components.tsx
@@ -21,7 +21,7 @@ const customLink = (classes: string, props: any) => {
   return (
     <a className={`${classes}`} target="_blank" rel="noopener noreferrer" {...props}>
       <span className="mr-1 break-words">{props.children}</span>
-      <svg viewBox="0 0 24 24" className="h-5 w-5 inline-block mb-1">
+      <svg viewBox="0 0 24 24" className="inline-block mb-1" height="16" width="16">
         <g fill="none" stroke="currentColor" strokeLinecap="round" strokeWidth="2">
           <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
           <path d="M15 3h6v6"></path>
