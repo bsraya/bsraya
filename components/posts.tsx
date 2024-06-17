0cbdcfb348ee3b3af2987f3d97ab69c25f012dd6
Bijon Setyawan Raya <bijonsetyawan@gmail.com>
2024-06-17 13:37:12 +0800

    style: adjust font size and weight for every device

--- components/posts.tsx
+++ components/posts.tsx
@@ -19,7 +19,7 @@ export default function Posts({ posts }: { posts: Post[] }) {
                                 href={`/posts/${post.slugAsParams}`}
                                 className="w-fit hover:underline"
                             >
-                                <h1 className="font-merriweather text-2xl">{post.title}</h1>
+                                <h1 className="font-merriweather font-thin md:text-2xl text-xl">{post.title}</h1>
                             </Link>
                             <p className="font-khula text-lg mt-auto text-slate-500">{post.description}</p>
                         </div>
--- components/works.tsx
+++ components/works.tsx
@@ -15,7 +15,7 @@ export default function Works({ works }: { works: Work[] }) {
                                 href={`/works/${work.slugAsParams}`}
                                 className="w-fit hover:underline"
                             >
-                                <h1 className="font-merriweather text-2xl">{work.title}</h1>
+                                <h1 className="font-merriweather font-thin md:text-2xl text-xl">{work.title}</h1>
                             </Link>
                             <p className="font-khula text-lg mt-auto text-slate-500">{work.description}</p>
                         </div>
