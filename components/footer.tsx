0d62909261c28437ebd53f0286cf423136a2b334
Bijon Setyawan Raya <bijonsetyawan@gmail.com>
2024-06-17 15:51:43 +0800

    style: change footer's font

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="font-baskervville mt-10 mb-2">
+        <footer className="font-khula mt-10 mb-2">
             © Bijon Setyawan Raya {new Date().getFullYear()}
         </footer>
     )
