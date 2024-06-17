eb088024f01c716d2d4d5e8e93ca2076ef5a8143
Bijon Setyawan Raya <bijonsetyawan@gmail.com>
2024-06-17 18:19:07 +0800

    style: adjust font weight

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -43,13 +43,13 @@ export default function Experience() {
                     <div className="flex flex-col gap-10">
                         <div>
                             <p className="font-lalezar text-lg text-slate-500">October 2022 - Present</p>
-                            <p className="font-merriweather text-3xl mb-1">Fullstack Developer</p>
-                            <p className="font-khula text-lg">Faria Education Group - Taipei, Taiwan</p>
+                            <p className="font-merriweather font-thin text-3xl mb-1">Fullstack Developer</p>
+                            <p className="font-khula text-lg text-slate-500">Faria Education Group - Taipei, Taiwan</p>
                         </div>
                         <div>
                             <p className="font-lalezar text-lg text-slate-500">February 2021 - December 2022</p>
-                            <p className="font-merriweather text-3xl mb-1">Graduate Research Assistant</p>
-                            <p className="font-khula text-lg">National Tsing Hua University - Hsinchu, Taiwan</p>
+                            <p className="font-merriweather font-thin text-3xl mb-1">Graduate Research Assistant</p>
+                            <p className="font-khula text-lg text-slate-500">National Tsing Hua University - Hsinchu, Taiwan</p>
                         </div>
                     </div>
                 </div>
@@ -59,13 +59,13 @@ export default function Experience() {
                     <div className="flex flex-col gap-10">
                         <div>
                             <p className="font-lalezar text-lg text-slate-500">February 2021 - January 2023</p>
-                            <p className="font-merriweather text-3xl mb-1">M. Sc. in Computer Science</p>
-                            <p className="font-khula text-lg">National Tsing Hua University - Hsinchu, Taiwan</p>
+                            <p className="font-merriweather font-thin text-3xl mb-1">M. Sc. in Computer Science</p>
+                            <p className="font-khula text-lg text-slate-500">National Tsing Hua University - Hsinchu, Taiwan</p>
                         </div>
                         <div>
                             <p className="font-lalezar text-lg text-slate-500">September 2015 - January 2021</p>
-                            <p className="font-merriweather text-3xl mb-1">B. Sc. in Computer Science</p>
-                            <p className="font-khula text-lg">National Tsing Hua University - Hsinchu, Taiwan</p>
+                            <p className="font-merriweather font-thin text-3xl mb-1">B. Sc. in Computer Science</p>
+                            <p className="font-khula text-lg text-slate-500">National Tsing Hua University - Hsinchu, Taiwan</p>
                         </div>
                     </div>
                 </div>
@@ -75,8 +75,8 @@ export default function Experience() {
                     <div className="flex flex-col gap-10">
                         <div>
                             <p className="font-lalezar text-lg text-slate-500">December 2018</p>
-                            <p className="font-merriweather text-3xl mb-1">Civil IoT Taiwan 2018</p>
-                            <p className="font-khula text-lg">Third place - Taipei, Taiwan</p>
+                            <p className="font-merriweather font-thin text-3xl mb-1">Civil IoT Taiwan 2018</p>
+                            <p className="font-khula text-lg text-slate-500">Third place - Taipei, Taiwan</p>
                         </div>
                     </div>
                 </div>
