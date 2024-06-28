4d1e69dbc60ce7783f7452e6b91ab630c97dfd5b
Bijon Setyawan Raya <bijonsetyawan@gmail.com>
2024-06-28 13:16:06 +0800

    refactor

--- components/header.tsx
+++ components/header.tsx
@@ -4,7 +4,9 @@ import BurgerMenu from './burger-menu'
 export default function Header() {
     return (
         <div className="flex pb-10">
-            <div className="font-khula font-bold md:text-[2rem] text-[1.25rem]"><Link href="/">BSR</Link></div>
+            <div className="font-khula font-bold md:text-[2rem] text-[1.25rem]">
+                <Link href="/">BSR</Link>
+            </div>
             <div className='flex ml-auto items-center'>
                 <BurgerMenu />
             </div>
