d3bb87ae20224491ceeec5352dccbc83c9739409
Bijon Setyawan Raya <bijonsetyawan@gmail.com>
2024-10-11 14:55:42 +0800

    others: code spacing

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,12 +6,12 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function getHeadings(text: string) {
-  let headings: {[key: string]: string[]} = {};
+  let headings: { [key: string]: string[] } = {};
   let match = null;
   const regex = /\#{2,6}\s(.*)$/gm;
   let currentKey = null;
   let currentArray = [];
-  
+
   while ((match = regex.exec(text)) !== null) {
     if (match[0].startsWith("## ")) {
       currentKey = match[0].replace("## ", "").trim();
