987f28c2cbd332e41981059e642065b2f528f64d
Bijon Setyawan Raya <bijonsetyawan@gmail.com>
2025-05-07 10:21:04 +0800

    others: move project to another repo

--- components/footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return (
-        <footer className="font-khula mt-10 mb-2">
-            © Bijon Setyawan Raya {new Date().getFullYear()}
-        </footer>
-    )
-}
